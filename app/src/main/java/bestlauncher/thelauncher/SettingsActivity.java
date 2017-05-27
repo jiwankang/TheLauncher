8aafb67db923fb1dd6acc4729080cfa2b7e29a33
jiwankang <j74kang@edu.uwaterloo.ca>
2017-05-27 14:57:57 -0400

    'labbutton'

--- app/src/main/java/bestlauncher/thelauncher/SettingsActivity.java
+++ app/src/main/java/bestlauncher/thelauncher/SettingsActivity.java
@@ -5,10 +5,17 @@ import android.app.KeyguardManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 
 public class SettingsActivity extends Activity {
@@ -33,5 +40,28 @@ public class SettingsActivity extends Activity {
     content = new TextView(getApplicationContext());
     content.setText("I’ve replaced the label");
     rl.addView(content);
+
+        Button createButton = new Button(getApplicationContext());
+        createButton.setText("Create File");
+        createButton.setOnClickListener(new View.OnClickListener() {
+            float x,y,z;
+            public void onClick(View v) {
+                try {
+                    File file = new File(getExternalFilesDir("aaa"), "Lab1.csv");
+                    FileWriter writer = new FileWriter(file.getAbsoluteFile());
+                    PrintWriter printWriter = new PrintWriter(writer);
+                    for(int i=0; i<100; i++){
+                        x = fileContents[i][0];
+                        y = fileContents[i][1];
+                        z = fileContents[i][2];
+                        printWriter.println(String.format("%f,%f,%f", x,y,z));
+                    }
+                    printWriter.close();
+                } catch (IOException e) {
+
+                }
+            }
+        });
+        rl.addView( (Button) createButton);
 }}
 
