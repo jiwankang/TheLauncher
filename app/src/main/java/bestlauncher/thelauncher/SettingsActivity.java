c4ad19219670f86424b7ad61edd31752d9f9f538
udallmo <udall_mo@outlook.com>
2017-05-27 06:54:52 -0400

    settings

--- app/src/main/java/bestlauncher/thelauncher/SettingsActivity.java
+++ app/src/main/java/bestlauncher/thelauncher/SettingsActivity.java
@@ -5,19 +5,35 @@ import android.app.KeyguardManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class SettingsActivity extends Activity {
+    TextView content;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.settings);
+        LinearLayout rl = (LinearLayout)findViewById(R.id.layout);
+        rl.setOrientation(LinearLayout.VERTICAL);
         getWindow().setType(WindowManager.LayoutParams.TYPE_KEYGUARD_DIALOG);
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED|WindowManager.LayoutParams.FLAG_FULLSCREEN|
                 WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
-        startService(new Intent(this, LockScreenService.class).setAction(Intent.ACTION_SCREEN_OFF));
-        setContentView(R.layout.settings);
+//        startService(new Intent(this, LockScreenService.class).setAction(Intent.ACTION_SCREEN_OFF));
+//        setContentView(R.layout.settings);
 
+        startService(new Intent(this,LockScreenService.class));
+        content = new TextView(this);
+        content.setText("APP NAME");
+        rl.addView(content);
+
+
+try {
+    content = new TextView(getApplicationContext());
+    content.setText("I’ve replaced the label");
+    rl.addView(content);
+}catch (Exception e) {
     }
-}
+}}
 
